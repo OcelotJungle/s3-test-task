2ee47ed1dabe5829a7578ca26fe5b6d9bc84c31d
OcelotJungle <ocelotjungle@yandex.ru>
2021-10-29 18:19:25 +0500

    Added extension validation

--- src/configs/file.config.ts
+++ src/configs/file.config.ts
@@ -2,5 +2,6 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("file", () => ({
     maxSize: parseInt(process.env.FILE_MAX_SIZE_IN_MB) * 1024 * 1024,
+    allowedExtensions: process.env.FILE_ALLOWED_EXTENSIONS.trim().replace(/\s+?/g, " "),
     allowedMimeTypes: process.env.FILE_ALLOWED_MIME_TYPES.trim().replace(/\s+?/g, " ")
 }));
--- src/validation/validation.service.ts
+++ src/validation/validation.service.ts
@@ -8,7 +8,7 @@ export class ValidationService {
 
     validateFile(file: Express.Multer.File): ValidationResult {
         if(!this.validateFileSize(file.size)) return ValidationResult.TOO_BIG;
-        if(!this.validateFileMimeType(file.mimetype)) return ValidationResult.NOT_ALLOWED;
+        if(!this.validateFileExtension(file.originalname) || !this.validateFileMimeType(file.mimetype)) return ValidationResult.NOT_ALLOWED;
         return ValidationResult.OK;
     }
 
@@ -16,6 +16,20 @@ export class ValidationService {
         // console.log({ size, maxSize });
 
         return maxSize === 0 || size <= maxSize;
+    }
+
+    validateFileExtension(filename: string, allowedExtensions: string = this.configService.get<string>("file.allowedExtensions")): boolean {
+        const splittedFilename = filename.split(".");
+        const extension = splittedFilename[splittedFilename.length - 1];
+
+        // console.log({ extension, allowedExtensions });
+
+        if(allowedExtensions.length === 0) return true;
+
+        const allowed = allowedExtensions.includes(extension);
+        const restricted = allowedExtensions.includes(`-${extension}`);
+
+        return allowed && !restricted;
     }
 
     validateFileMimeType(mimetype: string, allowedMimeTypes: string = this.configService.get<string>("file.allowedMimeTypes")): boolean {
